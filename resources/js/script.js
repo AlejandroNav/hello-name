74f7dbcd5954a882856f4b42488b23ffee714183
Alex-Nav <alejugger@gmail.com>
2021-03-22 11:51:41 -0600

    finished a nice canva rnadom particles

--- resources/js/script.js
+++ resources/js/script.js
@@ -2,6 +2,8 @@ const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
+const particulMatriz = [];
+const colorin = 'LightBlue'
 
 window.addEventListener('resize', function () {
     canvas.width = window.innerWidth;
@@ -15,45 +17,58 @@ const mouse = {
 canvas.addEventListener('click', function (event) {
     mouse.x = event.x
     mouse.y = event.y
-    drawCircle();
 })
+
 canvas.addEventListener('mousemove', function (event) {
     mouse.x = event.x
     mouse.y = event.y
-    drawCircle();
 })
-
-function drawCircle() {
-    context.fillStyle = 'blue';
-    context.beginPath();
-    context.arc(mouse.x, mouse.y, 20, 0, Math.PI * 2);
-    context.fill();
-}
-
 
 class Particle {
     constructor() {
         this.x = mouse.x;
         this.y = mouse.y;
-        this.size = Math.random() * 6 + 3;
-        this.speedX = Math.random() * 3 - 1.5;
-        this.speedY = Math.random() * 3 - 1.5;
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 5 + 1;
+        this.speedX = Math.random() * .2 - .1;
+        this.speedY = Math.random() * .4 - .01;
     }
-    update(){
+    update() {
         this.x += this.speedX
-        this.y += this.speedy
+        this.y += this.speedY
     }
-    draw(){
+    draw() {
+        context.fillStyle = colorin;
+        context.strokeStyle = 'red';
+        context.lineWidth = 2;
+        context.beginPath();
+        context.arc(this.x, this.y, this.size, 0, Math.PI * 2);
+        context.fill();
+        context.stroke();
+    }
+}
 
+ function init() {
+     for (let index = 0; index < 1500; index++) {
+         particulMatriz.push(new Particle());
+     }
+
+ }
+ init();
+
+function handleParticles() {
+    for (let i = 0; i < 1500; i++) {
+        particulMatriz[i].update();
+        particulMatriz[i].draw();
+        console.log(particulMatriz[i]);
     }
 
 }
 
 function animate() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    //     drawCicle();
-    drawCircle();
+    handleParticles();
     requestAnimationFrame(animate)
-    console.log("si funciona");
 }
 animate();
