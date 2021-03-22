c35701983678113a2ad2b62168b8756132bf9079
Alex-Nav <alejugger@gmail.com>
2021-03-21 23:00:42 -0600

    basic circle

--- resources/js/script.js
+++ resources/js/script.js
@@ -1,6 +1,15 @@
 const canvas = document.getElementById('canvas1');
 const context = canvas.getContext('2d');
-console.log(canvas);
-console.log(context);
-context.fillStyle = 'white';
-context.fillRect(10,10,100,100);
+
+window.addEventListener('resize', function () {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+})
+context.fillStyle = 'red';
+context.strokeStyle = 'red';
+context.lineWidth = 25;
+context.beginPath();
+context.arc(650, 250, 120, 0, Math.PI * 2);
+context.stroke();
+
