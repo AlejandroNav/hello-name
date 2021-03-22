0c442e0371a562ee47869aaa4d7a7e4c19b865d9
Alex-Nav <alejugger@gmail.com>
2021-03-21 22:14:11 -0600

    start Canvas files

---
+++ resources/js/script.js
@@ -0,0 +1,6 @@
+const canvas = document.getElementById('canvas1');
+const context = canvas.getContext('2d');
+console.log(canvas);
+console.log(context);
+context.fillStyle = 'white';
+context.fillRect(10,10,100,100);
