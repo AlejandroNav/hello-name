9021f4dd986f01b61edd53caff03616470881d1e
Alex-Nav <alejugger@gmail.com>
2021-03-21 23:52:07 -0600

    brush in canvas

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,10 +6,32 @@ window.addEventListener('resize', function () {
     canvas.height = window.innerHeight;
 
 })
-context.fillStyle = 'red';
+const mouse = {
+    x:null,
+    y:null,
+}
+canvas.addEventListener('click', function (event) {
+    mouse.x=event.x
+    mouse.y=event.y
+    drawCircle(mouse.x,mouse.y);
+})
+canvas.addEventListener('mousemove', function (event) {
+    mouse.x=event.x
+    mouse.y=event.y
+    drawCircle(mouse.x,mouse.y);
+})
+function drawCircle(eq,ye) {
 context.strokeStyle = 'red';
-context.lineWidth = 25;
+context.lineWidth = 2;
 context.beginPath();
-context.arc(650, 250, 120, 0, Math.PI * 2);
+context.arc(eq, ye, 10, 0, Math.PI * 2);
 context.stroke();
+}
+function randomCoord() {
+   const x = Math.ceil(Math.random() * 900 +50)
+   return x
+}
 
+for (let index = 0; index < 40; index++) {
+    drawCircle(randomCoord(),randomCoord())
+}
